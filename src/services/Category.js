2b147fc9098028ccfe2f1bc07a4bf79b9ada02ba
Ghaith <ghaith990000@gmail.com>
2023-11-15 12:40:47 +0300

    created addCategoryToMenu method

--- src/services/Category.js
+++ src/services/Category.js
@@ -1,6 +1,31 @@
 import {firestore} from "../firebase";
-import { collection, addDoc, query, where, getDocs, doc, getDoc } from "firebase/firestore";
+import { collection, addDoc, query, where, getDocs, doc, getDoc, updateDoc, arrayUnion} from "firebase/firestore";
 
-export const addCategoryToMenu = async(menuData) => {
-    
+const collectionName = 'menus';
+
+const generateCategoryId= () => {
+    return doc(collection(firestore, collectionName)).id;
+};
+
+export const addCategoryToMenu = async(menuId, categoryData) => {
+    try {
+        const menuDocRef = doc(firestore, collectionName, menuId);
+
+        // Generate a unique category ID
+        const categoryId = generateCategoryId();
+
+
+        // Add the new category to the 'categories' array using arrayUnion
+        await updateDoc(menuDocRef,{
+            [`categories.${categoryId}`]: {
+                name: categoryData.name,
+                description: categoryData.description,
+                image: categoryData.image,
+            },
+        });
+
+        console.log("Category added to the menu successfully.");
+    }catch(error){
+        console.log("Error adding category to the menu: ", error);
+    }
 }
